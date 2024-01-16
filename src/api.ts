06ec52557421f2fab7009ffe45209ce832f8a5e4
Nicolas Baldoino <nicolasbaldoino@outlook.com>
2024-01-15 21:22:08 -0300

    feat: implement ftp system

--- src/api.ts
+++ src/api.ts
@@ -1,24 +1,93 @@
-import express from 'express';
-import cors from 'cors';
+import { Client } from 'basic-ftp'
+import { File } from 'buffer'
+import cors from 'cors'
+import express from 'express'
+import fs from 'fs/promises'
+import os from 'os'
+import path from 'path'
 
-export const app = express();
+export const app = express()
 
-app.use(cors({ origin: true }));
+app.use(cors({ origin: true }))
 
-app.use(express.json());
-app.use(express.raw({ type: 'application/vnd.custom-type' }));
-app.use(express.text({ type: 'text/html' }));
+app.use(express.json())
+app.use(express.raw({ type: 'application/vnd.custom-type' }))
+app.use(express.text({ type: 'text/html' }))
 
-// Healthcheck endpoint
 app.get('/', (req, res) => {
-  res.status(200).send({ status: 'ok' });
-});
+  res.status(200).send({ status: 'ok' })
+})
 
-const api = express.Router();
+app.post('/', async (req, res) => {
+  const { file, remotePath } = req.body as { file: File; remotePath: string }
 
-api.get('/hello', (req, res) => {
-  res.status(200).send({ message: 'hello world' });
-});
+  const name = `${Math.random().toString(36).slice(-8)}.${file.type.split('/')[1]}`
 
-// Version the api
-app.use('/api/v1', api);
+  const tempStoragePath = path.join(os.tmpdir(), name)
+
+  const buffer = Buffer.from(await file.arrayBuffer())
+
+  const ftp = new Client()
+
+  try {
+    await fs.writeFile(tempStoragePath, buffer)
+
+    await ftp.access({
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASS,
+      secure: false,
+      secureOptions: { timeout: 100000 },
+    })
+
+    await ftp.cd(process.env.FTP_PATH || '/')
+
+    await ftp.cd(remotePath)
+
+    const info = await ftp.uploadFrom(tempStoragePath, name)
+
+    console.info('File sent:', info)
+
+    const url = `${process.env.FTP_URL}/${remotePath}/${name}`
+
+    res.status(200).send({ url })
+  } catch (error) {
+    console.error('Error Uploading File:', error)
+
+    res.status(500).send(error)
+  } finally {
+    await fs.unlink(tempStoragePath)
+
+    ftp.close()
+  }
+})
+
+app.delete('/', async (req, res) => {
+  const { url } = req.body as { url: string }
+
+  const ftp = new Client()
+
+  try {
+    ftp.access({
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASS,
+      secure: false,
+      secureOptions: { timeout: 100000 },
+    })
+
+    await ftp.cd(process.env.FTP_PATH || '/')
+
+    const path = url.replace(`${process.env.FTP_URL}/`, '')
+
+    const info = await ftp.remove(path)
+
+    res.status(200).send(info)
+  } catch (error) {
+    console.error('Error Deleting File:', error)
+
+    res.status(500).send(error)
+  } finally {
+    ftp.close()
+  }
+})
