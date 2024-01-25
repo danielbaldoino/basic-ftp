10b8b4654d4a7e75aa16590b36d82adaba245588
Nicolas Baldoino <nicolasbaldoino@outlook.com>
2024-01-24 21:36:13 -0300

    feat: implement signed url

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import fileUpload from 'express-fileupload'
+import jwt from 'jsonwebtoken'
 import { FtpOptions, ftpUpload } from './utils/ftp'
 
 export const app = express()
@@ -13,11 +14,50 @@ app.use(express.text({ type: 'text/html' }))
 
 app.use(fileUpload({ limits: { files: 1, fileSize: 50 * 1024 * 1024 } }))
 
+const verifyCredential = (req, res, next) => {
+  const credential = req.query.credential
+
+  if (!credential) {
+    return res.status(403).json({ error: 'Credential not provided' })
+  }
+
+  jwt.verify(credential, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Credential invalid' })
+    }
+
+    console.log(decoded)
+
+    req.decoded = decoded
+    next()
+  })
+}
+
 app.get('/', (req, res) => {
   res.status(200).send({ status: 'ok' })
 })
 
-app.post('/', async (req, res) => {
+app.post('/signed-url', (req, res) => {
+  const { host, password, user, path, remotePath, url } = req.body
+
+  if (!host || !password || !user) {
+    res.status(400).send({ error: 'Bad request' })
+    return
+  }
+
+  const credential = { host, password, user, path, remotePath, url }
+
+  const signedJwt = jwt.sign(credential, process.env.SECRET_KEY, {
+    expiresIn: '1m',
+  })
+
+  const signedUrl =
+    'https://' + req.get('host') + '/upload?credential=' + signedJwt
+
+  res.json(signedUrl)
+})
+
+app.post('/', verifyCredential, async (req, res) => {
   const file = req.files.file as fileUpload.UploadedFile
 
   if (!file) {
@@ -33,6 +73,8 @@ app.post('/', async (req, res) => {
   }
 
   try {
+    console.log('credential', (req as any).decoded)
+
     const url = await ftpUpload(file, options)
 
     if (url) res.status(200).send({ url })
